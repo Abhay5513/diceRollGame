db23c7908a24b02a53c69ebe860c74285221d08a
Abhay5513 <117533337+Abhay5513@users.noreply.github.com>
2023-03-16 17:24:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const diceEl = document.querySelector(".dice");
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add("hidden");
+const current0El = document.querySelector("#current--0");
+const current1El = document.querySelector("#current--1");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+let currentScore = 0;
+let activePlayer = 0;
+let scores = [0, 0];
+let playing = true;
+const inIt = function () {
+  // inilization function to reset the game.
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  currentScore = 0;
+  scores = [0, 0];
+  playing = true;
+  diceEl.classList.add("hidden");
+};
+const switchPlayer = function () {
+  //switch player function.
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+};
+btnRoll.addEventListener("click", function () {
+  if (playing) {
+    //Generating a random number.
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
+    //Display Dice roll.
+    diceEl.classList.remove("hidden");
+    diceEl.src = `dice-${dice}.png`;
+    //Add dice to current score.
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+btnHold.addEventListener("click", function () {
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    if (scores[activePlayer] >= 30) {
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove("player--active");
+    } else {
+      //Switch player.
+      switchPlayer();
+    }
+  }
+});
+
+//adding inilization functionality to the New Game button.
+btnNew.addEventListener("click", inIt);
